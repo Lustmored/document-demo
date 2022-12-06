352b80c8044e486610f099d27083ba8c5257625f
Jakub Caban <kuba.iluvatar@gmail.com>
2022-12-06 14:30:57 +0100

    4 basic examples of argument resolving

---
+++ src/Controller/MultipleFilesResolved.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Zenstruck\Document\Library\Bridge\Symfony\ValueResolver\UploadedFile;
+use Zenstruck\Document\PendingDocument;
+
+#[Route('/multiple_files', name: 'multiple_files')]
+class MultipleFilesResolved extends AbstractController
+{
+    public function __invoke(
+        #[UploadedFile('data[file]')]
+        array $files = []
+    ): Response {
+        return $this->render('multiple_files.html.twig', [
+            'files' => $files
+        ]);
+    }
+}
